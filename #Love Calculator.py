36b3e35059c6e55495ee7c5bf3272ee6585c1311
Alauddin Bukhari <56410694+Alauddinbukhari@users.noreply.github.com>
2023-03-02 11:49:56 +0530

    Update #Love Calculator.py

--- #Love Calculator.py
+++ #Love Calculator.py
@@ -1,28 +1,38 @@
-#Love Calculator
+# Print a welcome message
 print('Welcome to the Love Calculator')
-name1=input('What is your name?\n')
-name2=input('What is their name?')
 
-comname=name1+name2
-lowername=comname.lower()
-t=lowername.count("t")
-r=lowername.count("r")
-u=lowername.count('u')
-e=lowername.count('e')
- 
-true=t+r+u+e
+# Ask the user for the two names to be used in the love calculation
+name1 = input('What is your name?\n')
+name2 = input('What is their name?')
 
-l=lowername.count("l")
-o=lowername.count("o")
-v=lowername.count('v')
-e=lowername.count('e')
+# Concatenate the two names and convert the resulting string to lowercase
+comname = name1 + name2
+lowername = comname.lower()
 
-love=l+o+v+e
-lovescore1=str(true)+str(love)
-lovescore=int(lovescore1)
-if (lovescore<10) or (lovescore>90):
-    print(f"Your Love score is {lovescore},you go together like coke and mentos .")
-elif lovescore>=40 and lovescore<=50:
-    print(f"Your score is {lovescore},  you are allright with each other")
+# Calculate the number of occurrences of each letter in the string and add up the counts for 'true'
+t = lowername.count("t")
+r = lowername.count("r")
+u = lowername.count('u')
+e = lowername.count('e')
+true = t + r + u + e
+
+# Calculate the number of occurrences of each letter in the string and add up the counts for 'love'
+l = lowername.count("l")
+o = lowername.count("o")
+v = lowername.count('v')
+e = lowername.count('e')
+love = l + o + v + e
+
+# Combine the 'true' and 'love' scores to get a love score as a string
+lovescore1 = str(true) + str(love)
+
+# Convert the love score string to an integer
+lovescore = int(lovescore1)
+
+# Print a message to the user based on their love score
+if (lovescore < 10) or (lovescore > 90):
+    print(f"Your Love score is {lovescore}, you go together like coke and mentos.")
+elif lovescore >= 40 and lovescore <= 50:
+    print(f"Your score is {lovescore}, you are allright with each other.")
 else:
-    print(f'Your score is {lovescore}')
+    print(f'Your score is {lovescore}.')
